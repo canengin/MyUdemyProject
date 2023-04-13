b4e8817e3670a7c19a26e0c8975a17ea1a0d1229
Can Engin <kcanengin@gmail.com>
2023-04-13 16:19:57 +0300

    Api Controller'larına CRUD işlemleri tanımlandı.

--- ApiConsume/HotelProject.DataAccessLayer/Repositories/GenericRepository.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Repositories/GenericRepository.cs
@@ -29,6 +29,7 @@ namespace HotelProject.DataAccessLayer.Repositories
         public List<T> GetList()
         {
             return _context.Set<T>().ToList();
+
         }
 
         public void Insert(T t)
